bfe7b3d8efeaaa58e91587b7ab41ef0f5eb129f0
gabhiinav <gabhiinav@gmail.com>
2023-07-01 18:27:10 +0530

    CSS

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,17 @@
 export default function Home() {
-  return <div className="text-blue-700 font-bold">Hello World!</div>;
+  return (
+    <div className="w-ful h-full">
+      <div className="flex flex-col justify-center items-center gap-[2rem] container">
+        <div className="pt-[4rem] text-center text-white">
+          <div className="text-[32px] sm:text-[48px] lg:text-[72px] font-bold">
+            asmr sounds
+          </div>
+          <div>
+            a soothing blend of continuous, harmonious sounds for a peaceful
+            background ambiance.
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
